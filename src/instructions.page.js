6dba1f002c93b931d82ba25ba5558ad177d673ad
Andrew Vagliano <andrewvagliano2021@u.northwestern.edu>
2020-02-21 15:30:07 -0500

    Step By Step Reading

--- src/instructions.page.js
+++ src/instructions.page.js
@@ -7,38 +7,45 @@ import * as Speech from 'expo-speech';
 const InstructionsPage = ({ navigation }) => {
     const [buttonActive, setbuttonActive] = useState({ active: 0 });
 
-    const speak = async (text, time) => {
-        setTimeout(() => {
-            Speech.pause();
-        }, time);
-
-        setTimeout(() => {
-            Speech.resume();
-            Speech.speak(text);
-        }, time);
-        return null;
-    }
-
     const instructions = [
         "Apply a very generous, non-transparent layer to clean skin, thoroughly covering entire face", 
         "Allow to remain on for five to ten minutes",
         "Rinse thoroughly with warm water"
     ];
 
-    const Speak = () => {
+    const SBSInstructions = [
+        {
+            'text': "Apply a very generous, non-transparent layer to clean skin, thoroughly covering entire face",
+            'pause': 5000
+        },
+        {
+            'text': "Allow to remain on for five to ten minutes",
+            'pause': 500
+        },
+        {
+            'text': "I will set a five minute timer for you",
+            'pause': 10000
+        },
+        {
+            'text': "beep beep beep beep, your five minutes are up",
+            'pause': 500
+        },
+        {
+            'text': "Rinse thoroughly with warm water",
+            'pause': 5000
+        }
+    ]
+
+    const StepByStep = (instr) => {
+        if (instr.length == 0) { return; }
+
         setbuttonActive({ active: 2});
-        Speech.speak(instructions[0], {onDone:() => {
-            setTimeout(() => {
-                Speech.speak(instructions[1], {onDone:()=> {
-                    Speech.speak("I will set a five minute timer for you!")
-                    setTimeout(() => {
-                        Speech.speak("BEEP BEEP BEEP BEEP BEEP")
-                        Speech.speak('Your five minutes are up')
-                        Speech.speak(instructions[2]);
-                    }, 10000)
-                }});
-            }, 5000);
-        }})
+        Speech.speak(instr[0]['text'], {
+            onDone: () => {
+                Speech.pause();
+                setTimeout(() => StepByStep(instr.slice(1, instr.length)), instr[0]['pause']);
+            }
+        })
     };
 
     const ReadDirn = () => {
@@ -68,7 +75,7 @@ const InstructionsPage = ({ navigation }) => {
             </View>            
             <View style={styles.buttons}>
                 <TouchableOpacity onPress={ReadDirn} style={buttonActive.active==1? styles.pressedButton : styles.Button}><Text style={styles.buttonText}>Read Directions</Text></TouchableOpacity>
-                <TouchableOpacity onPress={Speak} style={buttonActive.active==2? styles.pressedButton : styles.Button}><Text style={styles.buttonText}>Step-by-step</Text></TouchableOpacity>
+                <TouchableOpacity onPress={() => StepByStep(SBSInstructions)} style={buttonActive.active==2? styles.pressedButton : styles.Button}><Text style={styles.buttonText}>Step-by-step</Text></TouchableOpacity>
                 <TouchableOpacity onPress={Tutorial} style={buttonActive.active==3? styles.pressedButton : styles.Button}><Text style={styles.buttonText}>Tutorial</Text></TouchableOpacity>
             </View>            
         </View>
