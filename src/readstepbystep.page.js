488d46654828b88939dbe8eacc7b01f49e0cff7b
sadiejhood <sadiehood2020@u.northwestern.edu>
2020-02-23 17:02:53 -0600

    navigate back to the instruciton page

--- src/readstepbystep.page.js
+++ src/readstepbystep.page.js
@@ -6,6 +6,7 @@ import * as Speech from 'expo-speech';
 
 const ReadStepByStepPage = ({ navigation, route }) => {
     const [currInstruction, setCurrInstruction] = useState(0);
+    const [done, setDone] = useState(false);
     const instructions = VanityData[route.params.key]['raw_instructions'];
 
     Speech.speak(instructions[currInstruction]);
@@ -13,18 +14,29 @@ const ReadStepByStepPage = ({ navigation, route }) => {
     const NextStep = () => {
         Speech.stop();
         setCurrInstruction(currInstruction + 1);
-        if (currInstruction == instructions.length) {
-            console.log('youre done');
+        if (currInstruction >= instructions.length - 1) {
+            console.log("here");
+            setDone(true);
         }
-        Speech.speak(instructions[currInstruction]);
+        // Speech.speak(instructions[currInstruction + 1]);
     }
 
-    return (
-        <View>
-            <Title>{instructions[currInstruction]}</Title>
-            <Button onPress={NextStep}>Next Step</Button>
-        </View>
-    )
+    if (!done) {
+        return (
+            <View>
+                <Title>{instructions[currInstruction]}</Title>
+                <Button onPress={NextStep}>Next Step</Button>
+            </View>
+        )
+    } else {
+        Speech.speak("You have finished all the steps! Click below to return to the instruction page.");
+        return (
+            <View>
+                <Title>You have finished all the steps! Click below to return to the instruction page.</Title>
+                <Button onPress={() => navigation.navigate('Instructions', {'key':route.params.key})}>Back to Instruction Page</Button>
+            </View>
+        )
+    }
 }
 
 export default ReadStepByStepPage;
