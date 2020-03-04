b728f2dc678504312f6bc9f42f7b96dd52dd2ae5
Andrew Vagliano <andrewvagliano2021@u.northwestern.edu>
2020-03-04 18:41:10 -0500

    removed intro button

--- src/intro.page.js
+++ src/intro.page.js
@@ -17,10 +17,9 @@ const IntroPage = ({navigation}) => {
               style={styles.introBG}
             >
                 <Text style={{position:'absolute', top: '50%', left: '30%', fontSize: '40%', color: '#FFFFFF', fontWeight: 'bold'}}>BeautyQ</Text>
-                {/* <Button style={styles.introButton} mode='flat' color='#ffffff' onPress={moveToNextPage()}>Get Started</Button> */}
-                <Button style={{top: '90%', width: '60%', marginLeft: '20%', borderBottomColor: 'white', borderBottomWidth: 1}} onPress={()=>{navigation.navigate('Vanity'), {'navigated': false}}}>
+                {/* <Button style={{top: '90%', width: '60%', marginLeft: '20%', borderBottomColor: 'white', borderBottomWidth: 1}} onPress={()=>{navigation.navigate('Vanity'), {'navigated': false}}}>
                     <Text style={{color: '#FFF'}}>Click Here to Continue</Text>
-                </Button>
+                </Button> */}
             </ImageBackground>
         </View>
     )
